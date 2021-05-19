5eb79a404df7d548b2f02cacf1e9167f624d4ba0
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-05-19 21:34:20 +0200

    menus dropdown completados

--- js/main.js
+++ js/main.js
@@ -7,7 +7,11 @@ function seleccionarPais(ev){
     wrapSeleccionPais.classList.toggle("wrapSeleccionPaisnoOculta");
 }*/
 
+
+
 $(document).ready(function(){
+
+    let flagCajaFuera=false;    
 
     $("#flip").click(function(){
        
@@ -16,14 +20,107 @@ $(document).ready(function(){
         document.getElementById("crosspais").classList.toggle('fa-angle-up',true);
     });
 
+    function cerrarMenuDropdown(){
+        if (!($('.box-empresa').is('close'))){
+            console.log('cierro');
+            $('.box-empresa').addClass('close');
+            $('.box-empresa').css('display','none');
+            $('#empresa').removeClass('iluminar');
+        }
+        if (!($('.box-contacto').is('close'))){
+            console.log('cierro');
+            $('.box-contacto').addClass('close');
+            $('.box-contacto').css('display','none');
+            $('#contacto').removeClass('iluminar');
+        }
+        if (!($('.box-cpr').is('close'))){
+            console.log('cierro');
+            $('.box-cpr').addClass('close');
+            $('.box-cpr').css('display','none');
+            $('#cpr').removeClass('iluminar');
+        }
+        if (!($('.box-producto').is('close'))){
+            console.log('cierro');
+            $('.box-producto').addClass('close');
+            $('.box-producto').css('display','none');
+            $('#productos').removeClass('iluminar');
+        }
+    }
+
     $("#empresa").on('click', function(event){
         event.preventDefault();
-        console.log("despliega");
-
+        
+        if ($('.box-empresa').is('.close')){
+            console.log("despliega");
+            cerrarMenuDropdown();
+            $('.box-empresa').css('display','flex');
+            $('.box-empresa').removeClass('close');
+            $('#empresa').addClass('iluminar');
+            //$(this).addClass('open');
+        }else{
+            console.log("repliega");
+            cerrarMenuDropdown();
+        }
         $('#empresa i').removeClass('fa-angle-down');
         $('#empresa i').addClass('fa-times');
-        $('#box-empresa').slideToggle('slow');  
+        /*$('#box-empresa').slideToggle(); */
                  
     })
+    $("#productos").on('click', function(event){
+        event.preventDefault();
 
+        if ($('.box-producto').is('.close')){
+            console.log("despliega");
+            cerrarMenuDropdown();
+            $('.box-producto').css('display','flex');
+            $('.box-producto').removeClass('close');
+            $('#productos').addClass('iluminar');
+            //$(this).addClass('open');
+        }else{
+            console.log("repliega");
+            cerrarMenuDropdown();
+        }
+        $('#productos i').removeClass('fa-angle-down');
+        $('#productos i').addClass('fa-times');
+        /*$('#box-empresa').slideToggle(); */
+                 
+    })
+    $("#contacto").on('click', function(event){
+        event.preventDefault();
+
+        if ($('.box-contacto').is('.close')){
+            console.log("despliega");
+            cerrarMenuDropdown();
+            $('.box-contacto').css('display','flex');
+            $('.box-contacto').removeClass('close');
+            $('#contacto').addClass('iluminar');
+            //$(this).addClass('open');
+        }else{
+            console.log("repliega");
+            cerrarMenuDropdown();
+        }
+        $('#contacto i').removeClass('fa-angle-down');
+        $('#contacto i').addClass('fa-times');
+        /*$('#box-empresa').slideToggle(); */
+                 
+    })
+    $("#cpr").on('click', function(event){
+        event.preventDefault();
+
+        if ($('.box-cpr').is('.close')){
+            console.log("despliega");
+            cerrarMenuDropdown();
+            $('.box-cpr').css('display','flex');
+            $('.box-cpr').removeClass('close');
+            $('#cpr').addClass('iluminar');
+            //$(this).addClass('open');
+        }else{
+            console.log("repliega");
+            cerrarMenuDropdown();
+        }
+        $('#cpr i').removeClass('fa-angle-down');
+        $('#cpr i').addClass('fa-times');
+        /*$('#box-empresa').slideToggle(); */
+                 
+    })
 })
